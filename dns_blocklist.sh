751d95571886481b6dac1197741dc0f9f96a47a6
Kyle Hill <kylhill@gmail.com>
2021-04-21 13:52:33 -0500

    Update to use newer unbound blocking mechanism, whitelist roku and echo

--- dns_blocklist.sh
+++ dns_blocklist.sh
@@ -4,8 +4,8 @@
 # Create a local-zone block list for unbound #
 ##############################################
 
-#DNS_RETURN="always_nxdomain"
-DNS_RETURN="0.0.0.0"
+DNS_RETURN="always_null"
+#DNS_RETURN="0.0.0.0"
 
 CWD="/opt/dns_blocklist/"
 BLOCKLIST_GENERATOR=$CWD"generate-domains-blocklist.py"
@@ -21,7 +21,7 @@ clean_list() {
 }
 
 print_record() {
-    if [[ "$DNS_RETURN" == "deny" || "$DNS_RETURN" == "refuse" || "$DNS_RETURN" == "static" || "$DNS_RETURN" == "always_refuse" || "$DNS_RETURN" == "always_nxdomain" ]]; then
+    if [[ "$DNS_RETURN" == "deny" || "$DNS_RETURN" == "refuse" || "$DNS_RETURN" == "static" || "$DNS_RETURN" == "always_refuse" || "$DNS_RETURN" == "always_nxdomain" || "$DNS_RETURN" == "always_null" ]]; then
         awk -v rtn=$DNS_RETURN '{printf "local-zone: \"%s.\" %s\n", $1, rtn}'
     else
         awk -v rtn=$DNS_RETURN '{printf "local-zone: \"%s.\" redirect\nlocal-data: \"%s. 3600 IN A %s\"\n", $1, $1, rtn}'
